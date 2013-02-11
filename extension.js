46a0db49c065e7cf254274111f43f564c154dd04
Ben Burke <bhburke@umich.edu>
2013-02-11 01:15:42 -0500

    init commit

---
+++ extension.js
@@ -0,0 +1,53 @@
+$(function(){
+	console.log("page loaded getting tweets");
+	var tweet_box = $('a[data-element-term="tweet_stats"]');
+	var username = $(tweet_box).attr('href').substring(1);
+	get_tweets(username, tweet_box);
+
+});
+
+$(document).on('DOMNodeInserted', 'a[data-element-term="tweet_stats"]', function(){
+	console.log("new thing loaded");
+	var tweet_box = $('a[data-element-term="tweet_stats"]');
+	var username = $(tweet_box).attr('href').substring(1);
+	get_tweets(username, tweet_box);
+});
+
+function get_tweets(username, box){
+	console.log("started tweet getting");
+	var request = $.ajax({
+		url: "https://api.twitter.com/1/users/lookup.json",
+		type: "GET",
+		async: false,
+		data:{
+			"screen_name": username
+		},
+		dataType:"json",
+		success:function(response){
+			parse_tweets(response, box);
+		},
+		error:function(response){
+			console.log(response);
+		},
+	});
+
+	console.log(request);
+}
+
+function parse_tweets(response, box){
+	var firstDate = new Date();
+	var lastDate = new Date(response[0].created_at);
+	var difference = firstDate - lastDate;
+	console.log(difference);
+	var days = Math.round(difference/(1000*60*60*24));
+	console.log(days);
+	var num_tweets = response[0].statuses_count;
+
+	var ratio = Math.round(num_tweets/days*100)/100
+
+	console.log(num_tweets);
+	box.html("<strong>"+ratio+"</strong> Tweets / day");
+
+
+
+}
