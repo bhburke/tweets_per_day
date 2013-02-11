7b4b1d62a2f896346c38851089ea62f47df5f768
Connor Stack <cstack@umich.edu>
2013-02-11 13:06:35 -0500

    cache api responses

--- extension.js
+++ extension.js
@@ -1,8 +1,8 @@
 
-console.log("page loaded getting tweets");
+console.log("Page loaded. Getting tweets / day");
 var tweet_box = $('a[data-element-term="tweet_stats"]');
 var username = $(tweet_box).attr('href').substring(1);
-get_tweets(username, tweet_box);
+get_tweets_per_day(username, tweet_box);
 
 
 $(document).on('DOMNodeInserted', 'div#profile_popup', function(){
@@ -16,11 +16,27 @@ $(document).on('DOMNodeInserted', 'div#profile_popup', function(){
 		console.log(tweet_box);
 	}
 	var username = $(tweet_box).attr('href').substring(1);
-	//get_tweets(username, tweet_box);
+	//get_tweets_per_day(username, tweet_box);
 });
 
-function get_tweets(username, box){
-	console.log("started tweet getting");
+// Modify box to show tweets/day for username
+function get_tweets_per_day(username, box){
+	chrome.storage.sync.get(username, function(data) {
+		var user_object = data[username];
+		
+		if (user_object == undefined) {
+			console.log(username+" is not cached. Perform api call");
+			make_api_call(username, box);
+		} else {
+			console.log(username+" is cached. Just use that info");
+			modify_box(user_object, box);
+		}
+	});
+}
+
+// Get info about username, then modify box
+function make_api_call(username, box) {
+	console.log("Querying API for "+username);
 	var request = $.ajax({
 		url: "https://api.twitter.com/1/users/lookup.json",
 		type: "GET",
@@ -30,7 +46,7 @@ function get_tweets(username, box){
 		},
 		dataType:"json",
 		success:function(response){
-			parse_tweets(response, box);
+			cache_response(username, response, box);
 		},
 		error:function(response){
 			console.log(response);
@@ -39,21 +55,41 @@ function get_tweets(username, box){
 
 }
 
-function parse_tweets(response, box){
-	var firstDate = new Date();
-	var lastDate = new Date(response[0].created_at);
-	var difference = firstDate - lastDate;
-	console.log(difference);
+// Save api response for username, then modify box
+function cache_response(username, response, box) {
+
+  // Check response is valid
+  if (response.length < 1) {
+  	console.log("Invalid response : "+response);
+  	return;
+  }
+
+  var user_object = response[0];
+  console.log("Caching "+username+" : ", user_object);;
+
+  // Save it using the Chrome extension storage API, then use that value
+  var data_to_save = {};
+  data_to_save[username] = user_object;
+  chrome.storage.sync.set(data_to_save, function() {
+  	console.log("Response cached");
+    modify_box(user_object, box);
+  });
+}
+
+// Modify box to show tweets/day for user_object
+function modify_box(user_object, box){
+
+	var today = new Date();
+	var created_at = new Date(user_object.created_at);
+	var difference = today - created_at;
 	var days = Math.round(difference/(1000*60*60*24));
 	console.log(days);
-	var num_tweets = response[0].statuses_count;
+	var num_tweets = user_object.statuses_count;
 
-	var ratio = Math.round(num_tweets/days*100)/100
-
+	var ratio = Math.round(num_tweets/days*100)/100;
+		
 	console.log(num_tweets);
 	box.attr("modified", "1");
 	box.html("<strong>"+ratio+"</strong> Tweets / day");
-
-
 
 }
